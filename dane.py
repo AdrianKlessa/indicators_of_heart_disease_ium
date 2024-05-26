6e4aa129179633fcafc2ac5b7b2b12f8695bcc0a
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2024-05-26 15:20:23 +0200

    Add files via upload

---
+++ dane.py
@@ -0,0 +1,148 @@
+### 1. Pobieranie zbioru danych
+import zipfile
+with zipfile.ZipFile("personal-key-indicators-of-heart-disease.zip", 'r') as zip_ref:
+    zip_ref.extractall("dataset_extracted")
+import pandas as pd
+# W pobranym zbiorze danych jest kilka podzbiorów więc celowo otwieram ten z NaN, żeby manualnie go oczyścić dla praktyki
+df = pd.read_csv("dataset_extracted/2022/heart_2022_with_nans.csv")
+## Przeglądanie nieoczyszczonego datasetu
+df.info()
+df.head()
+df.describe()
+df["HadHeartAttack"].value_counts().plot(kind="pie")
+df["HadHeartAttack"].value_counts()
+
+## 2. Podział na podzbiory (train / dev / test - 8:1:1)) i oversampling
+from sklearn.model_selection import train_test_split
+#Funkcji z sklearn musimy użyć dwukrotnie, bo dzieli tylko na dwa podzbiory
+train, test_and_valid = train_test_split(df, test_size=0.2) #0.8 train, 0.2 test&valid
+
+test, valid = train_test_split(test_and_valid, test_size=0.5) #0.1 test, 0.1 valid
+train["HadHeartAttack"].value_counts()
+def oversample(dataset):
+    num_true = len(dataset[dataset["HadHeartAttack"]=="Yes"])
+    num_false = len(dataset[dataset["HadHeartAttack"]=="No"])
+    num_oversampling_steps = num_false//num_true
+    oversampled = dataset.copy()
+    for x in range(num_oversampling_steps):
+        oversampled = pd.concat([oversampled, dataset[dataset["HadHeartAttack"]=="Yes"]], ignore_index=True)
+    return oversampled
+train = oversample(train)
+train["HadHeartAttack"].value_counts().plot(kind="pie")
+test["HadHeartAttack"].value_counts().plot(kind="pie")
+valid["HadHeartAttack"].value_counts().plot(kind="pie")
+df["SmokerStatus"].value_counts().plot(kind="pie")
+df["ECigaretteUsage"].value_counts().plot(kind="pie")
+df["CovidPos"].value_counts().plot(kind="pie")
+## Normalizacja część 1 - zamiana na kolumny liczbowe i kategoryczne
+df["Sex"].unique()
+df["GeneralHealth"].unique()
+health_map = {
+    "Excellent": 5,
+    "Very good": 4,
+    "Good": 3,
+    "Fair": 2,
+    "Poor": 1
+}
+for col in df:
+    print(f"{col}:")
+    print(df[col].unique())
+from collections import defaultdict
+def normalize_dataset(dataset):
+    dataset["GeneralHealth"] = dataset["GeneralHealth"].map(defaultdict(lambda: float('NaN'), health_map), na_action='ignore')
+    dataset["Sex"] = dataset["Sex"].map({"Female":0,"Male":1}).astype(float) #Zamiana z kolumn tekstowych na numeryczne
+    dataset.rename(columns ={"Sex":"Male"},inplace=True)
+    dataset["State"] = dataset["State"].astype('category')
+    dataset["PhysicalHealthDays"].astype(float)
+    dataset["MentalHealthDays"].astype(float)
+    dataset["LastCheckupTime"] = dataset["LastCheckupTime"].fillna("Unknown").astype('category') # Potem korzystam z fillna-->median ale nie działa to na kolumnach kategorycznych więc wykonuję to przed konwersją
+    dataset["PhysicalActivities"]= dataset["PhysicalActivities"].map({"No":0,"Yes":1})
+    dataset["SleepHours"].astype(float)
+    dataset["RemovedTeeth"] = dataset["RemovedTeeth"].map(defaultdict(lambda: float('NaN'), {"None of them":0,"1 to 5":1, "6 or more, but not all":2, "All":3}), na_action='ignore')
+    dataset["HadHeartAttack"]= dataset["HadHeartAttack"].map({"No":0,"Yes":1})
+    dataset["HadAngina"]= dataset["HadAngina"].map({"No":0,"Yes":1})
+    dataset["HadStroke"]= dataset["HadStroke"].map({"No":0,"Yes":1})
+    dataset["HadAsthma"]= dataset["HadAsthma"].map({"No":0,"Yes":1})
+    dataset["HadSkinCancer"]= dataset["HadSkinCancer"].map({"No":0,"Yes":1})
+    dataset["HadCOPD"]= dataset["HadCOPD"].map({"No":0,"Yes":1})
+    dataset["HadDepressiveDisorder"]= dataset["HadDepressiveDisorder"].map({"No":0,"Yes":1})
+    dataset["HadKidneyDisease"]= dataset["HadKidneyDisease"].map({"No":0,"Yes":1})
+    dataset["HadArthritis"]= dataset["HadArthritis"].map({"No":0,"Yes":1})
+    dataset["HadDiabetes"]= dataset["HadDiabetes"].map({"No":0,"Yes, but only during pregnancy (female)":1,"No, pre-diabetes or borderline diabetes":2,"Yes":3})
+
+    dataset["DeafOrHardOfHearing"]= dataset["DeafOrHardOfHearing"].map({"No":0,"Yes":1})
+    dataset["BlindOrVisionDifficulty"]= dataset["BlindOrVisionDifficulty"].map({"No":0,"Yes":1})
+    dataset["DifficultyConcentrating"]= dataset["DifficultyConcentrating"].map({"No":0,"Yes":1})
+    dataset["DifficultyWalking"]= dataset["DifficultyWalking"].map({"No":0,"Yes":1})
+    dataset["DifficultyDressingBathing"]= dataset["DifficultyDressingBathing"].map({"No":0,"Yes":1})
+    dataset["DifficultyErrands"]= dataset["DifficultyErrands"].map({"No":0,"Yes":1})
+    dataset["SmokerStatus"]= dataset["SmokerStatus"].map({"Never smoked":0,"Current smoker - now smokes some days":1,"Former smoker":2,"Current smoker - now smokes every day":3})
+    dataset["ECigaretteUsage"]= dataset["ECigaretteUsage"].map({"Never used e-cigarettes in my entire life":0,"Not at all (right now)":1,"Use them some days":2,"Use them every day":3})
+    dataset["ChestScan"]= dataset["ChestScan"].map({"No":0,"Yes":1})
+    dataset["RaceEthnicityCategory"] = dataset["RaceEthnicityCategory"].fillna("Unknown").astype('category')
+    dataset["AgeCategory"] = dataset["AgeCategory"].fillna("Unknown").astype('category')
+    dataset["HeightInMeters"] = dataset["HeightInMeters"].astype(float)
+    dataset["WeightInKilograms"] = dataset["WeightInKilograms"].astype(float)
+    dataset["BMI"] = dataset["BMI"].astype(float)
+    dataset["AlcoholDrinkers"]= dataset["AlcoholDrinkers"].map({"No":0,"Yes":1})
+    dataset["HIVTesting"]= dataset["HIVTesting"].map({"No":0,"Yes":1})
+    dataset["FluVaxLast12"]= dataset["FluVaxLast12"].map({"No":0,"Yes":1})
+    dataset["PneumoVaxEver"]= dataset["PneumoVaxEver"].map({"No":0,"Yes":1})
+    dataset["TetanusLast10Tdap"]= dataset["TetanusLast10Tdap"].apply(lambda x: float('NaN') if type(x)!=str else 1.0 if 'Yes,' in x else 1.0 if 'No,' in x else float('NaN'))
+    dataset["HighRiskLastYear"]= dataset["HighRiskLastYear"].map({"No":0,"Yes":1})
+    dataset["CovidPos"]= dataset["CovidPos"].map({"No":0,"Yes":1})
+test.head()
+normalize_dataset(test)
+test.head()
+test.info()
+normalize_dataset(train)
+normalize_dataset(valid)
+train.describe()
+test.describe()
+valid.describe()
+import seaborn as sns
+sns.set_theme()
+g = sns.catplot(
+    data=train, kind="bar",
+    x="GeneralHealth", y="WeightInKilograms", hue="HadHeartAttack",
+    errorbar="sd", palette="dark", alpha=.6, height=6
+)
+g.despine(left=True)
+g.set_axis_labels("General health index", "Body mass (kg)")
+g.legend.set_title("Had heart attack")
+valid.groupby('SmokerStatus', as_index=False)['HadHeartAttack'].mean()
+valid.groupby('GeneralHealth', as_index=False)['HadHeartAttack'].mean()
+valid.pivot_table('HadHeartAttack',index='GeneralHealth', columns='SmokerStatus')
+## Normalizacja część 2 - Skalowanie kolumn numerycznych do 0-1
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+def scale_float_columns(dataset):
+    numerical_columns = list(dataset.select_dtypes(include=['float64']).columns)
+    dataset[numerical_columns] = scaler.fit_transform(dataset[numerical_columns])
+test.head()
+scale_float_columns(test)
+scale_float_columns(train)
+scale_float_columns(valid)
+test.head()
+## 5. Czyszczenie brakujących pól
+print(df.shape[0])
+print(df.shape[0] - df.dropna().shape[0])
+test.head()
+
+numeric_columns = train.select_dtypes(include=['number']).columns
+test[numeric_columns] = test[numeric_columns].fillna(test[numeric_columns].median().iloc[0])
+train[numeric_columns] = train[numeric_columns].fillna(train[numeric_columns].median().iloc[0])
+valid[numeric_columns] = valid[numeric_columns].fillna(valid[numeric_columns].median().iloc[0])
+
+test.head()
+test["HighRiskLastYear"].value_counts()
+test["HighRiskLastYear"].isna().sum()
+test.info()
+train.info()
+valid.info()
+
+cat_columns = test.select_dtypes(['category']).columns
+
+test.to_csv("test.csv")
+train.to_csv("train.csv")
+valid.to_csv("valid.csv")
---
+++ evaluate.py
@@ -0,0 +1,82 @@
+import pandas as pd
+valid = pd.read_csv("valid.csv")
+
+x_columns = ['Male', 'GeneralHealth', 'PhysicalHealthDays', 'MentalHealthDays',
+       'PhysicalActivities', 'SleepHours', 'RemovedTeeth',
+       'HadAngina', 'HadStroke', 'HadAsthma', 'HadSkinCancer', 'HadCOPD',
+       'HadDepressiveDisorder', 'HadKidneyDisease', 'HadArthritis',
+       'HadDiabetes', 'DeafOrHardOfHearing', 'BlindOrVisionDifficulty',
+       'DifficultyConcentrating', 'DifficultyWalking',
+       'DifficultyDressingBathing', 'DifficultyErrands', 'SmokerStatus',
+       'ECigaretteUsage', 'ChestScan', 'HeightInMeters', 'WeightInKilograms',
+       'BMI', 'AlcoholDrinkers', 'HIVTesting', 'FluVaxLast12', 'PneumoVaxEver',
+       'TetanusLast10Tdap', 'HighRiskLastYear', 'CovidPos']
+y_column = 'HadHeartAttack'
+
+
+valid_x = valid[x_columns]
+valid_y = valid[y_column]
+
+from tensorflow import keras
+model = keras.models.load_model('model.keras')
+
+import numpy as np
+predictions = model.predict(valid_x)[:,0]
+true_answers = valid_y.to_numpy()
+validation_accuracy = np.sum(np.rint(predictions) == true_answers)/len(true_answers)
+print(f"Poprawność na zbiorze walidacyjnym: {validation_accuracy:.2%}")
+
+np.savetxt("predictions.txt",predictions)
+np.savetxt("predictions_two_digits.txt",predictions, fmt='%1.2f')
+
+validate_heart_disease_true = valid.loc[valid[y_column]==1]
+validate_heart_disease_false = valid.loc[valid[y_column]==0]
+
+from datetime import timezone
+import datetime
+import json
+
+validate_heart_disease_true_x = validate_heart_disease_true[x_columns]
+validate_heart_disease_false_x = validate_heart_disease_false[x_columns]
+
+
+
+predictions_for_true = model.predict(validate_heart_disease_true_x)[:,0]
+predictions_for_false = model.predict(validate_heart_disease_false_x)[:,0]
+
+true_positives = np.sum(np.rint(predictions_for_true) == np.ones_like(predictions_for_true)).tolist()
+true_negatives = np.sum(np.rint(predictions_for_false) == np.zeros_like(predictions_for_false)).tolist()
+false_positives = len(predictions_for_false)-true_negatives
+false_negatives = len(predictions_for_true)-true_positives
+
+
+current_datetime = datetime.datetime.now(timezone.utc)
+metrics = {"true_positives": true_positives, "true_negatives": true_negatives, "false_positives": false_positives, "false_negatives" : false_negatives, "datetime_utc" : str(current_datetime)}
+history = []
+try:
+    with open("metrics.json","r") as f:
+       history = json.load(f)
+except FileNotFoundError:
+    print('No historical metrics found')
+
+history.append(metrics)
+with open("metrics.json","w") as f:
+       json.dump(history, f)
+
+
+import matplotlib.pyplot as plt
+true_positives_history = [x["true_positives"] for x in history]
+true_negatives_history = [x["true_negatives"] for x in history]
+false_positives_history = [x["false_positives"] for x in history]
+false_negatives_history = [x["false_negatives"] for x in history]
+
+plt.plot(true_positives_history, "-go")
+plt.plot(true_negatives_history, "-bo")
+plt.plot(false_positives_history, "-ro")
+plt.plot(false_negatives_history, "-mo")
+
+plt.legend(["True positives", "True negatives", "False positives", "False negatives"])
+plt.xlabel("Build number")
+plt.ylabel("Metric value")
+plt.title("Model evaluation history")
+plt.savefig("metrics.jpg")
